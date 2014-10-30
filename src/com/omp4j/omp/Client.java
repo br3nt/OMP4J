1a5409b54fe7eebc7f6595f5c7230bf73225d910
br3nt <brent.jacobs@gmail.com>
2014-10-30 21:00:08 +1030

    First commit
    This commit contains a very basic implementation of a Java OMP implementation.
    
    The first commit is to test getConfig() to ensure we can connect to OpenVAS and
    receive a successful response message.

---
+++ src/com/omp4j/omp/Client.java
@@ -0,0 +1,59 @@
+package com.omp4j.omp;
+
+import com.proc.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Brent Jacobs
+ */
+public class Client {
+    
+    private final String username, password, host, port;
+    
+    public Client(String username, String password, String host, String port) {
+        this.username = username;
+        this.password = password;
+        this.host = host;
+        this.port = port;
+    }
+    
+    public Client(String username, String password) {
+        this(username, password, "localhost", "9390");
+    }
+    
+    private String ompCommand() {
+        return "omp";
+    }
+    
+    private String connectionParameters() {
+        return "-u " + username + " -w " + password + " -h " + host + " -p " + port;
+    }
+    
+    public Proc getCommand(String command) {
+        return new Proc(ompCommand() + " " + connectionParameters() + " " + command);
+    }
+    
+    public String getConfigs() throws IOException {
+        Proc cmd = getCommand("--xml=\"<get_configs/>\" -i");
+        return cmd.exec();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String... args) {
+        try {
+            Client omp = new Client("admin", "password");
+            
+            String output = omp.getConfigs();
+            System.out.println("===  Get Configs ==================");
+            System.out.println(output);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
